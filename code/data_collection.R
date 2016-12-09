61cfed7352fd93bcc46d1c1254894640021fd3b9
sahilthapar <sahilthapar1992@gmail.com>
2016-12-09 16:42:10 -0500

    Added data preparation

--- code/data_collection.R
+++ code/data_collection.R
@@ -1,15 +1,22 @@
-library("RSQLite")
-library("dplyr")
-library("readr")
-library("tidyverse")
-library("tibble")
-library('stringr')
-library('lubridate')
+# @knitr library
+library("RSQLite") # Connecting to RSQLite
+library("dplyr")   # Working
+library("readr")   # Reading and writing files
+library("tidyverse") # For all the tidying functions from the tidy universe
+library("tibble")    # For working with tibbles
+library('stringr')   # Working with strings and regex
+library('lubridate') # Working with dates
+library('purrr')
+library(XML)
 
+# @knitr download_data
 # Download the file to temp folder
 temp <- tempfile()
-download.file("https://kaggle2.blob.core.windows.net/datasets/63/589/database.sqlite.zip",
-              temp, method = 'curl')
+download.file(
+  "https://kaggle2.blob.core.windows.net/datasets/63/589/database.sqlite.zip?sv=2015-12-11&sr=b&sig=btA9gYGczWR6SFFq6rG84Eok8jazkwa8JNwfYKAGtAc%3D&se=2016-12-12T16%3A25%3A12Z&sp=r",
+  temp,
+  method = 'curl'
+)
 
 # Extract to get the sqlite file
 data <- unzip(temp)
@@ -17,6 +24,7 @@ filename <- str_c(temp, str_sub(data, 2))
 
 # Connect to the sqlite file
 con = dbConnect(RSQLite::SQLite(), dbname = unzip(temp))
+con = dbConnect(RSQLite::SQLite(), dbname = 'data/database.sqlite')
 # get a list of all tables
 dbListTables(con)
 
@@ -37,10 +45,10 @@ myQuery <- dbSendQuery(con, "SELECT * FROM Team")
 Team <- dbFetch(myQuery, n = -1)
 # Clean up the environment
 rm(temp, filename, data, con, myQuery)
-unlink(temp)
+
 
 # Find the country id for England's Premier League
-England_id <- League %>% 
+England_id <- League %>%
   filter(name == "England Premier League") %>%
   select(id) %>%
   as.double()
@@ -52,41 +60,75 @@ Match <- Match %>%
 
 # Clean up data
 # Match table has a season column - this should be a factor variable
-# Also the date is only 
+# Also the date is only
 # Result is defined as:
-  # if home_goal > away_goal : home
-  # else if home_goal == away_goal : draw
-  # else away
+# if home_goal > away_goal : home
+# else if home_goal == away_goal : draw
+# else away
 
-getResult <- function(home_goal, away_goal){
+getResult <- function(home_goal, away_goal) {
   return (ifelse(home_goal > away_goal,
                  'home',
-                 (ifelse(home_goal == away_goal,
-                         'draw', 'away'))
-  ))
+                 (
+                   ifelse(home_goal == away_goal,
+                          'draw', 'away')
+                 )))
 }
 
 
 
-tidy_Match <- Match %>%
-  mutate(season = (season %>% factor(levels = unique(season))),
-         date = as.Date(date),
-         stage = (stage %>% factor(levels = c(1:38))),
-         result =  getResult(home_team_goal, away_team_goal)) %>% 
+tidy_match <- Match %>%
+  mutate(
+    season = (season %>% factor(levels = unique(season))),
+    date = as.Date(date),
+    stage = (stage %>% factor(levels = c(1:38))),
+    result =  getResult(home_team_goal, away_team_goal)
+  ) %>%
   mutate(result = factor(result, levels = c("home", "draw", "away"))) %>%
   select(matches("id|season|stage|date|result|team_goal|goal"))
 
+convertXMLtoDFAndAddMatchID <- function(goal_xml, match_id) {
+  df <- xmlToDataFrame(goal_xml)
+  if (nrow(df) > 0) {
+    return(cbind(df, match_id = match_id))
+  }
+  return(df)
+}
+
+Goals <- tidy_match$goal %>%
+  map2(tidy_match$match_api_id, convertXMLtoDFAndAddMatchID) %>%
+  bind_rows() %>%
+  as_tibble %>%
+  transmute(
+    type = fct_recode(
+      comment,
+      "normal" = "n",
+      "penalty" = "p",
+      "disallowed goal" = "dg",
+      "own goal" = "o",
+      "penalty saved" = "npm",
+      "penalty missed" = "psm",
+      "retake penalty" = "rp"
+    ),
+    elapsed = as.integer(elapsed),
+    player1 = as.integer(player1),
+    player2 = as.integer(player2),
+    subtype = factor(subtype, levels = unique(subtype)),
+    team = as.integer(team),
+    match_id = match_id
+  )
+
 tidy_Match %>%
   group_by(result) %>%
-  summarize(perc = (n()/nrow(.)) * 100) 
+  summarize(perc = (n() / nrow(.)) * 100)
 
 
-# tmp <- str_match_all(x, "<goal_type>(.+?)</goal_type>")
+# tmp <- str_match_all(x, "<goal_type>(.+?)<//goal_type>")
 # tmp_goal_type <- unlist(sapply(tmp,
 #                                   function(y){
 #                                     return(str_c(y[,2], collapse = '|'))
 #                                   }))
-# 
+#
 # tmp <- str_match_all(tidy_Match$goal, "<subtype>(.+?)</subtype>")
 # tmp_goal_subtype <- unlist(sapply(tmp,
 #                            function(y){
@@ -94,4 +136,3 @@ tidy_Match %>%
 #                              }))
 # tidy_Match$goal_type <- tmp_goal_type
 # tidy_Match$goal_subtype <- tmp_goal_subtype
-
